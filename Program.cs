4eebf7b5831eb934ca644a9ab3049d714f5f631e
Tehpson <jesper@familjen-pettersson.se>
2020-10-05 13:39:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jeopardy_par_programering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
