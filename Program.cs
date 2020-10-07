3d593c992385b9d10efa20e9d245d0892e385ebc
Tehpson <jesper@familjen-pettersson.se>
2020-10-07 10:18:53 +0200

    generate 6 redom cat form all the sesons

--- Program.cs
+++ Program.cs
@@ -9,18 +9,18 @@ namespace jeopardy_par_programering
     {
         static void Main(string[] args)
         {
-            bool sucseed = false;
-            string season;
 
-
-            while (!sucseed)
+            Console.WriteLine("Laoding...");
+            bool sucseed = questions.setupData();
+            Console.Clear();
+            if (!sucseed)
             {
-                Console.Write("witch sesong do you wanan play you can choos between season 1 and 36: \t");
-                season = Console.ReadLine();
-                //set up the data to match the season that was choosen
-                sucseed =  questions.setupData(season);
+                Console.WriteLine("ERROR while eting up data...");
             }
-            Console.WriteLine(questions.sixcat[1]);
+            
+
+
+
             // draw up the game board. 
             //ska vi gör aså att man kan välja hur många man 
             // vill köra liksom köra 2 eller flera. tänker att man bara gör funktioner som man kallar på.
--- questions.cs
+++ questions.cs
@@ -14,111 +14,70 @@ namespace jeopardy_par_programering
         /*  TO DO:
          *  make rendom function
          *  make get all where category is / take one of each value where cetegory is
-         *  store all active filedata/queston to make it easier to get the answer and give out points ang not care about inrelvant "qustions"
+         *  store all active dataList/queston to make it easier to get the answer and give out points ang not care about inrelvant "qustions"
          
         */
 
 
-        public static List<data> filedata = new List<data>();
+        public static List<data> dataList = new List<data>();
         public static List<string> sixcat = new List<string>();
-        public static bool setupData(string season)
+        public static bool setupData()
         {
             
             bool sucseed;
+
             //get the path to the rigth diractery
             string workingDirectory, projectDirectory, path;
             workingDirectory = Environment.CurrentDirectory;
             projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
             path = Path.Combine(projectDirectory, "jeopardy_clue_dataset");
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
 
-            //--------takes jsut hte numbers in teh string that was inserted by player
-            string seasonB = string.Empty;
-            int seasonNumber = -1; // -1 makes so if no numbers was enterd the program would just go to catch in later stages
-            for (int i = 0; i < season.Length; i++)
-            {
-                if (Char.IsDigit(season[i]))
-                    seasonB += season[i];
-            }
-
-            if (seasonB.Length > 0)
-                seasonNumber = int.Parse(seasonB);
-            //-------------------------
-
-
-            //this is under try jsut to see if this work else the player have enterd an invalid season and have to redo it.
+            
             try
             {
-                path = Path.Combine(projectDirectory, "jeopardy_clue_dataset");
-                path = Path.Combine(path, "season"+ seasonNumber + ".tsv");
-
-                string[] file = File.ReadAllLines(path);
-                
-                string[] row;
-
-                for (int i = 1; i < file.Length; i++)
+                //for each file in teh diractoy that ends in TSV get the data and add it to the dataList.
+                foreach (FileInfo file in directoryInfo.GetFiles("*.tsv"))
                 {
-                    row = file[i].Split("\t");
-                    filedata.Add(new data
+                    string[] tsv = File.ReadAllLines(file.FullName);
+
+                    string[] row;
+
+                    for (int i = 1; i < tsv.Length; i++)
                     {
-                        value = row[1],
-                        daily_double = row[2],
-                        category = row[3],
-                        answer = row[5],
-                        question = row[6]
-                    });
+                        row = tsv[i].Split("\t");
+                        dataList.Add(new data
+                        {
+                            value = row[1],
+                            daily_double = row[2],
+                            category = row[3],
+                            answer = row[5],
+                            question = row[6]
+                        });
+                    }
                 }
 
-                //skulel kuna läga in hela funtionen här då vi bara kallar 
-                //på den en gång men vet inte hur runda två fungerar ifall 
-                //man ska ha 6st nya kateogirer därfölr jag gjorde en funktion för det
+
                 getSixCategories();
                 sucseed = true;
             }
             catch
             {
-                Console.WriteLine("looks like we don't have the seasong you choose from");
                 sucseed = false;
             }
-            
-            return sucseed;
 
+            return sucseed;
         }
+            
 
         public static void getSixCategories()
         {
             Random rnd = new Random();
 
-            List<string> categories = new List<string>();
+           
             
             //kollare igneom varje rad av datta vi har och tar kategorin
-            for (int i = 0; i < filedata.Count; i++)
-            {
-                string outcat = filedata[i].category;
-                //ifall vi itne har någon categori i våran lista av kategorier 
-                //så lägger vi till annars kolla vilka kalteogrier vi har
-                if (categories.Count == 0) 
-                {
-                    categories.Add(outcat);
-                }
-                else
-                {
-                    bool numCatExist = false;
-                    //kollar för varej katogori vi har ifall den stämmer överäns med den som fildata gav till oss.
-                    for (int y = 0; y < categories.Count; y++)
-                    {
-                        //ifall catekorin finns en ända gång säger vi att den finns
-                        if (categories[y].ToString() == outcat.ToString())
-                        {
-                            numCatExist = true;
-                        }
-                    }
-                    //ifall kategorin inte fanns lägger vi till den ananrs så går vi bara vidare
-                    if (!numCatExist)
-                    {
-                        categories.Add(outcat);
-                    }
-                }
-            }
+         
 
             // kollar så att vi får 6 katigorer  0 1 2 3 4 5 = 6st
             //för att inte råka få två av samam kategori tar vi simpelt bara
@@ -126,26 +85,55 @@ namespace jeopardy_par_programering
             //till den i listan med 6 katigorier
             while (sixcat.Count < 5)
             {
-                int RendomValue = rnd.Next(categories.Count);
-                sixcat.Add(categories[RendomValue]);
-                categories.RemoveAt(RendomValue);
+                int RendomValue = rnd.Next(dataList.Count);
+                //if there is no katigoris already then just add it else se if teh rendom cat already exist
+                if (sixcat.Count == 0)
+                {
+                    sixcat.Add(dataList[RendomValue].category);
+                }
+                else
+                {
+                    bool excist = false;
+                    int i = 0;
+                    //while category don't extist and we haven't lookd at all categry in out 6 once if it colide cheack else go past
+                    while(i < sixcat.Count && !excist)
+                    {
+                        if (sixcat[i] == dataList[RendomValue].category)
+                        {
+                            excist = true;
+                        }
+                        i++;
+                    }
+                    if (!excist)
+                    {
+                        sixcat.Add(dataList[RendomValue].category);
+                    }
+                    
+                }
+                
             }
 
+
+            //for debug jsut write out the 6 cat
+            /*
             for (int i = 0; i < sixcat.Count; i++)
             {
                 Console.WriteLine(sixcat[i]);
-            }
-        }
-
-        //need to be public so filedata can be public. this feels stupid but cant find a another way.
-        public class data
-        {
-            public string value { get; set; }
-            public string daily_double { get; set; }
-            public string category { get; set; }
-            public string answer { get; set; }
-            public string question { get; set; }
+            }*/
         }
 
     }
+
+
+
+    //just use this to get a list with this valus in it
+    class data
+    {
+        public string value { get; set; }
+        public string daily_double { get; set; }
+        public string category { get; set; }
+        public string answer { get; set; }
+        public string question { get; set; }
+    }
+
 }
