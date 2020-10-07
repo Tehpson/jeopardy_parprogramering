8d2fdd2121d3d1f66d568f0560c8f7811ac270c1
Philip Ekeberg <phi.ekeberg@gmail.com>
2020-10-07 14:10:44 +0200

    Börjat fundera på Main

--- Program.cs
+++ Program.cs
@@ -9,25 +9,50 @@ namespace jeopardy_par_programering
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.WriteLine("-----------------Welcome to Jeopardy!-----------------");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("\t How many players?");
+            string playerNum = Console.ReadLine();
 
-            Console.WriteLine("Laoding...");
+            Console.WriteLine("Loading...");
             bool sucseed = questions.setupData();
             Console.Clear();
             if (!sucseed)
             {
-                Console.WriteLine("ERROR while eting up data...");
+                Console.WriteLine("ERROR while eating up data...");
             }
-            
+
+            //Ska vi köra typ en sån här switch som hoppar in i antalet spelare? där antalet spelare som
+            //har en egen method som kallar på dem andra programmen baserat på antalet spelare?
+            switch (playerNum)
+            {
+                case "1":
+                    OnePlayer();
+                    return true;
+                case "2":
+                    TwoPlayers();
+                    return true;
+                case "3":
+                    ThreePlayers();
+                    return true;
+                case "4":
+                    FourPlayers();
+                    return true;
+                default:
+                    return true;
+            }
 
 
 
-            // draw up the game board. 
-            //ska vi gör aså att man kan välja hur många man 
-            // vill köra liksom köra 2 eller flera. tänker att man bara gör funktioner som man kallar på.
-            // eller om jag mins rätt så gäller det att vara snabbat oxå så vi typ gör att
-            //alla får varsin knapp på tangetbordet och sedan kollar man vilken knapp som blev nertryckt snabbast
-            // med hjälp av Console.ReadKey(); funktionen?
+                    // draw up the game board. 
+                    //ska vi gör aså att man kan välja hur många man 
+                    // vill köra liksom köra 2 eller flera. tänker att man bara gör funktioner som man kallar på.
+                    // eller om jag mins rätt så gäller det att vara snabbat oxå så vi typ gör att
+                    //alla får varsin knapp på tangetbordet och sedan kollar man vilken knapp som blev nertryckt snabbast
+                    // med hjälp av Console.ReadKey(); funktionen?
 
-        }
+            }
     }
 }
