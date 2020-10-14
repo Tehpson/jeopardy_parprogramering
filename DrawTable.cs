116bddf86482576f0277f6cfc921f874ec158637
Tehpson <jesper@familjen-pettersson.se>
2020-10-14 16:26:50 +0200

    added comments

--- DrawTable.cs
+++ DrawTable.cs
@@ -8,6 +8,8 @@ namespace jeopardy_par_programering
 {
     class DrawTable
     {
+
+        //diffreant stages of game
         enum Menu
         {
             Start, 
@@ -15,18 +17,26 @@ namespace jeopardy_par_programering
             PlayerName,
             board,
         }
+        //where cureses is
         private static int[] activebord = { 0, 1 };
         private static int activePlayerCount = 2;
+
+        //lsit of the playername that we send to game.cs
         private static List<string> players = new List<string>();
         public static void draw()
         {
-            
+            //set that we start with staret
             Menu state = Menu.Start;
+
+            //so the game dosn't cloase
             while (true)
             {
                 Console.Clear();
+
+                //see waht state we are in
                 switch (state)
                 {
+                    //just wirte out in the conosol
                     case Menu.Start:
 
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 50, 5); Console.Write(@"               _                            _               _                                _       ");
@@ -41,10 +51,11 @@ namespace jeopardy_par_programering
 
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 5, Console.WindowHeight - 10); Console.WriteLine(">  Start");
 
+                        //juat make this to have it wait for an input
                         _ = Console.ReadKey().Key;
                         break;
 
-
+                        //choos ho many players
                     case Menu.PlayerCount:
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 46, 5); Console.Write(@"  _    _                                                     _                          ___  ");
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 46, 6); Console.Write(@" | |  | |                                                   | |                        |__ \ ");
@@ -55,10 +66,11 @@ namespace jeopardy_par_programering
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 46, 11); Console.Write(@"                                                __/ | | |             __/ |                  ");
                         Console.SetCursorPosition((Console.WindowWidth / 2) - 46, 12); Console.Write(@"                                               |___/  |_|            |___/                   ");
 
+
                         for (int i = 2; i < 5;  i++)
                         {
-
                             int numberColum = (Console.WindowWidth / 4 * (i-1));
+                            //if the curser is on teh same as we write out jsut worte out > before 
                             if (activePlayerCount == i)
                             {
                                 Console.SetCursorPosition(numberColum - 4, Console.WindowHeight - 10);
@@ -70,12 +82,13 @@ namespace jeopardy_par_programering
                         break;
 
 
-
+                        //let teh player choos player name
                     case Menu.PlayerName:
                         for (int i = 0; i < activePlayerCount ; i++)
                         {
                             bool nameDone = false;
                             string name = "";
+                            //loop until the palyer have neterd full name
                             while (!nameDone)
                             {
                                 Console.Clear();
@@ -92,10 +105,13 @@ namespace jeopardy_par_programering
                                 Console.SetCursorPosition((Console.WindowWidth / 2) - name.Length / 2, Console.WindowHeight - 10); Console.WriteLine(name);
 
                                 var localinput = Console.ReadKey();
+                                //if the charcter is a letter at the charicter to the name
                                 if (Char.IsLetter(localinput.KeyChar))
                                 {
                                     name = name + localinput.Key;
-                                }else if (localinput.Key == ConsoleKey.Enter)
+
+                                } // if the inputed key is enter key stop teh loop and add teh name to the player list 
+                                else if (localinput.Key == ConsoleKey.Enter)
                                 {
                                     nameDone = true;
                                     players.Add(name);
@@ -106,8 +122,9 @@ namespace jeopardy_par_programering
 
 
 
-
+                        //draw the game board
                     case Menu.board:
+                        //for each cat
                         for (int i = 0; i < 6; i++)
                         {
                             int value = 0;
@@ -115,7 +132,7 @@ namespace jeopardy_par_programering
                             int pointcolum = (Console.WindowWidth / 7 * (i + 1) - 2);
                             Console.SetCursorPosition(catColum, Console.WindowHeight / 7);
                             Console.Write(questions.question_list[i].category);
-
+                            //for each row of question
                             for (int y = 1; y < 6; y++)
                             {
                                 switch (y)
@@ -136,7 +153,9 @@ namespace jeopardy_par_programering
                                         value = questions.dataList[questions.question_list[i].question5ID].value;
                                         break;
                                 }
+                                //if value is bettwen 600 and 1000 just reduce by 500 to get the rigth vlau. (in round two do the opesist)
                                 if (value > 500) value = value - 500;
+                                //if curser is on teh current question add > before it
                                 if (activebord[0] == i && activebord[1] == y)
                                 {
                                     Console.SetCursorPosition(pointcolum-4, Console.WindowHeight / 7 * (y + 1));
@@ -155,6 +174,7 @@ namespace jeopardy_par_programering
 
                 switch (state)
                 {
+                    //change state from start to plci kplayer count
                     case Menu.Start:
                         state = Menu.PlayerCount;
                         break;
@@ -164,12 +184,14 @@ namespace jeopardy_par_programering
                         var inputplyaer = Console.ReadKey().Key;
                         switch (inputplyaer)
                         {
+                            //if we press the rigth button then we cahnge the cursor one step
                             case ConsoleKey.RightArrow:
                                 if (activePlayerCount < 4)
                                 {
                                     activePlayerCount++;
                                 }
                                 break;
+                                // if we press teh left button change the cursor to the left one step
                             case ConsoleKey.LeftArrow:
                                 if (activePlayerCount > 2)
                                 {
@@ -177,18 +199,21 @@ namespace jeopardy_par_programering
                                 }
                                 break;
 
+                                //if enter conintu to plater name
                             case ConsoleKey.Enter:
                                 state = Menu.PlayerName;
                                 break;
                         }
                         break;
 
+                        //if all playername have been put in go to the game board
                     case Menu.PlayerName:
                         state = Menu.board;
                         break;
 
                     case Menu.board:
 
+                        //look witch way the player change the curser.
                         var inputboard = Console.ReadKey().Key;
                         switch (inputboard)
                         {
