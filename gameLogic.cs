424960858fcc4afe8b1a80b7d589a8165931849a
Philip Ekeberg <phi.ekeberg@gmail.com>
2020-10-16 09:53:29 +0200

    Moved some content from DrawTable to gameLogic and fixed some errors

--- gameLogic.cs
+++ gameLogic.cs
@@ -8,29 +8,33 @@ namespace jeopardy_par_programering
     {
         public static List<Player> players = new List<Player>();
 
+        //diffreant stages of game
+        enum Stage
+        {
+            Start,
+            PlayerCount,
+            PlayerName,
+            Board,
+            Answer,
+            Question,
+        }
+        //where cureses is
+        private static int[] activebord = { 0, 1 };
+        private static int activePlayerCount = 2;
+        private static int activeAnswerCuror = 0;
+        private static int qestionID;
+        private static string fastestPlayerName = "";
 
         /*      TODO:
          *      Function that add teh players to a playerlist
          *      Funtion that add or subtract score from the player
          */
-
-        //Börjat jobba med score function,
         //idk tror inte vi beöhver detta då jag sickar till dig spelare och value och ifall det var rätt eller fel.  gör istället en void function som tar in stirng Name, int Value, bool answer
-        /*string[,] categories_points = new string[5, 5]
-        {
-            {" $100  ", "  $100  ", "  $100  ", "  $100  ", "  $100  " },
-            {" $200  ", "  $200  ", "  $200  ", "  $200  ", "  $200  " },
-            {" $300  ", "  $300  ", "  $300  ", "  $300  ", "  $300  " },
-            {" $400  ", "  $400  ", "  $400  ", "  $400  ", "  $400  " },
-            {" $500  ", "  $500  ", "  $500  ", "  $500  ", "  $500  " }
-        };
-        string[] categories = new string[] { };//get categories from tsv file
-        int category = 0;*/
-
-
+        
         // här gör att du ahr imot en lista med namn istället, och sedan för List.count loppar du till du har laggt in alla 
-        //gör ändast void functioner då ci inte kommer behlva få tillbaka någon data när vi kllar på dem. (public static void addPlayers(List<string> playername))
-        public static void addPlayers(List<string> playername)
+        //gör ändast void functioner då ci inte kommer behlva få tillbaka någon data när vi kllar på dem.
+
+        /*public static void AddPlayers(List<string> playername)
         {
             foreach (var player in playername)
             {
@@ -38,26 +42,7 @@ namespace jeopardy_par_programering
                 players.Add(new Player { Name = player, Points = 0 });
             }
         }
-
-        // List<string> PlayerList = new List<string>();
-        //PlayerList.AddRange(name);
-        //return PlayerList;//hur ger man bäst ett return value på detta?
-        /*
-        var listName = new List<string>();
-
-        string playerName = "Philip";
-
-        listName.Add(playerName);
-
-        foreach (var item in listName)
-        {
-            Console.WriteLine(item);
-        }
-
-
     }*/
-        //Tänker en function som man kan kalla på för att se och jämföra poängen mellan de tävlande
-        //Typ som detta?
         // humm kan vara bra i slutet där man kollar vem som van. 
         public static int ComparePlayers(Player Name) { return 0; }
 
@@ -67,6 +52,147 @@ namespace jeopardy_par_programering
             
         }
 
+        public static void GameState()
+        {
+            Stage state = Stage.Start;
+
+            switch (state)
+            {
+                //change state from start to plci kplayer count
+                case Stage.Start:
+                    state = Stage.PlayerCount;
+                    break;
+
+                case Stage.PlayerCount:
+
+                    var inputPlayer = Console.ReadKey().Key;
+                    switch (inputPlayer)
+                    {
+                        //if we press the rigth button then we cahnge the cursor one step
+                        case ConsoleKey.RightArrow:
+                            if (activePlayerCount < 4)
+                            {
+                                activePlayerCount++;
+                            }
+                            break;
+                        // if we press teh left button change the cursor to the left one step
+                        case ConsoleKey.LeftArrow:
+                            if (activePlayerCount > 2)
+                            {
+                                activePlayerCount--;
+                            }
+                            break;
+
+                        //if enter conintu to plater name
+                        case ConsoleKey.Enter:
+                            state = Stage.PlayerName;
+                            break;
+                    }
+                    break;
+
+                //if all playername have been put in go to the game board
+                case Stage.PlayerName:
+                    state = Stage.Board;
+                    gameLogic.AddPlayers(players);
+                    break;
+
+
+                case Stage.Board:
+
+                    //look witch way the player change the curser.
+                    var inputboard = Console.ReadKey().Key;
+                    switch (inputboard)
+                    {
+                        case ConsoleKey.UpArrow:
+                            if (activebord[1] > 1)
+                            {
+                                activebord[1] = activebord[1] - 1;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (activebord[1] < 5)
+                            {
+                                activebord[1] = activebord[1] + 1;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (activebord[0] > 0)
+                            {
+                                activebord[0] = activebord[0] - 1;
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (activebord[0] < 5)
+                            {
+                                activebord[0] = activebord[0] + 1;
+                            }
+                            break;
+                        case ConsoleKey.Enter:
+                            var cat = activebord[0];
+                            var question = activebord[1];
+                            qestionID = 1;
+                            switch (question)
+                            {
+                                case 1:
+                                    qestionID = questions.question_list[cat].question1ID;
+                                    break;
+                                case 2:
+                                    qestionID = questions.question_list[cat].question2ID;
+                                    break;
+                                case 3:
+                                    qestionID = questions.question_list[cat].question3ID;
+                                    break;
+                                case 4:
+                                    qestionID = questions.question_list[cat].question4ID;
+                                    break;
+                                case 5:
+                                    qestionID = questions.question_list[cat].question5ID;
+                                    break;
+                            }
+                            state = Stage.Question;
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    break;
+
+                case Stage.Question:
+                    state = Stage.Answer;
+                    break;
+
+
+                case Stage.Answer:
+                    var answerInput = Console.ReadKey().Key;
+                    switch (answerInput)
+                    {
+                        case ConsoleKey.RightArrow:
+                            if (activeAnswerCuror == 0)
+                            {
+                                activeAnswerCuror++;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (activeAnswerCuror == 1)
+                            {
+                                activeAnswerCuror--;
+                            }
+                            break;
+                        case ConsoleKey.Enter:
+                            //sene to game.cs with playername and value of the question
+                            bool wasAnswerRigth = false;
+                            if (activeAnswerCuror == 0)
+                                wasAnswerRigth = false;
+                            else wasAnswerRigth = true;
+
+                            gameLogic.addScore(fastestPlayerName, questions.dataList[qestionID].value, wasAnswerRigth);
+                            state = Stage.Board;
+                            break;
+                    }
+                    break;
+            }
+        }
 
         /* public static List<data> ComparePlayers(Player listName, Player player2, Player player3, Player player4)
          {
